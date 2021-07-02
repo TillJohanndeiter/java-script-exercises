ac1242a07cd18f59aa6ce20125f7d1f28fb7cbb0
Till Johanndeiter <till.johanndeiter@web.de>
2021-07-02 23:01:37 +0200

    Add fractions.js

---
+++ week_10/fractions.js
@@ -0,0 +1,39 @@
+const LIMIT = 1001
+
+const sum = (accumulator, currentValue) => accumulator + currentValue[0]
+
+function* range(from, upTo) {
+    for (let i = from; i < upTo; i++) {
+        yield i
+    }
+}
+
+
+function ggt(z, n) {
+
+    if (z === 0) {
+        return n
+    } else {
+        while (n !== 0) {
+            if (z > n) {
+                z -= n
+            } else {
+                n -= z
+            }
+        }
+        return z
+    }
+}
+
+
+let uniqueFractions = []
+
+for (let n = 0; n < LIMIT; n++) {
+    for (let z of range(1, n)) {
+        if (ggt(z,n) === 1) {
+            uniqueFractions.push([z, n])
+        }
+    }
+}
+
+console.log(uniqueFractions.reduce(sum, 0))
